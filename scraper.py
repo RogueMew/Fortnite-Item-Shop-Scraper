30bf7eb7d728026098ba3e8bb2664d3674f94a5b
RogueMew <48804205+RogueMew@users.noreply.github.com>
2024-10-19 19:06:03 -0700

    QOL Updates

--- scraper.py
+++ scraper.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from tqdm import tqdm
 import requests as web
 import json
 import git
 import datetime
 import os
+
+
 
 def getDate() -> str:
     currentDateTime = datetime.datetime.now()
@@ -20,23 +23,18 @@ def checkSameDate(date) -> bool:
 def capFirst(string) ->str:
     return string[0].upper() + string[1:]
 
-def imageDownload(url, name):
-    if os.path.isdir(f'./images/{getDate()}') == False:
-        os.mkdir(f'./images/{getDate()}')
-    with open('images/' + f'{getDate()}/' + name.replace(' ', '-').replace('|', '-') + '.png', "wb") as f:
+def imageDownload(url, name, date):
+    if os.path.isdir(f'./images/{date}') == False:
+        os.mkdir(f'./images/{date}')
+    with open('images/' + f'{date}/' + name.replace(' ', '-').replace('|', '-') + '.png', "wb") as f:
         f.write(web.get(url).content)
 
-def updateReadMe() -> str:
+def updateReadMe(date) -> str:
     readme = open("README.md", "w", encoding="utf-8")
-    readme.write("# Fortnite Item Shop Historical Viewer\n")
-    readme.write(f"## [Toadys Shop as Markdown](https://github.com/RogueMew/Fortnite-Item-Shop-Historical/blob/main/Markdown/{getDate()}-ItemShop.md)- {getDate()}\n## [Todays Item Shop Images](https://github.com/RogueMew/Fortnite-Item-Shop-Historical/tree/main/images/{getDate()})")
+    readme.write(f"# Fortnite Item Shop Historical Viewer | {date}\n")
+    readme.write(f"## [Toadys Shop as Markdown](https://github.com/RogueMew/Fortnite-Item-Shop-Historical/blob/main/Markdown/{getDate()}-ItemShop.md)\n## [Todays Item Shop Images](https://github.com/RogueMew/Fortnite-Item-Shop-Historical/tree/main/images/{getDate()})")
     readme.close()
     return "README.md"
-
-def deleteItemsinFolder(folderName):
-    dirFiles = os.listdir(f'./images/{folderName}')
-    for img in dirFiles:
-        os.remove(f'./images/{getDate()}/' + img)
 
 def updateGitIgnore(fileName):
     file = open('.gitignore', 'a+')
@@ -44,12 +42,12 @@ def updateGitIgnore(fileName):
     if fileName not in file.read():
         file.write('\n' + fileName)
 
-def scraping()->None:
+def scraper()-> list:
     driver = webdriver.Edge()
     driver.get("https://www.fortnite.com/item-shop?lang=en-US&_data=routes%2Fitem-shop._index")
     content = json.loads(driver.find_element(By.CSS_SELECTOR, 'div[hidden="true"]').get_attribute('textContent'))
     driver.close()
-    print("Scraped Site, Compiling into Markdown and Downloading images")
+    print("Scraped Site")
     itemshop = []
     for catagory in content['catalog']['categories']:
         for section in catagory['sections']:
@@ -65,25 +63,61 @@ def scraping()->None:
                         "name" : item.get('title', None),
                         'catagory' : catagory['navLabel'],
                         'section' : section['displayName'],
-                        'type' : capFirst(item['assetType']),
+                        'type' : item['assetType'], 
                         'price' : str(item['pricing']['finalPrice']) + " vbucks",
                         'inDate' : inDate.split("T")[0],
                         'outDate' : outDate.split("T")[0],
                         'image' : item['image'].get('lg', None),
                         'urlExstension' : item.get('urlName', None),
-                        'imageLocal' : item['title'].replace(' ', '-').replace('|', '-') + '.png'
+                        'rmtImage' : item['image'].get('wide', None),
+                        'imageLocal' : item['title'].replace(' ', '-').replace('|', '-') + '.png',
+                        'hasVariants' : item.get('hasVariants', None)
                     }
-                    if item_dict['type'] == 'Dynamicbundle':item_dict['type'] = 'Bundle'
-                    if item_dict['type'] == 'Vmtpack':item_dict['type'] = 'Pack'
-                    if item_dict['type'] == 'Jamtrack':item_dict['type'] = 'Jam-Track'
-                    if item_dict['type'] == 'Legokits': item_dict['type'] = 'Lego-Kit'
+                    if item_dict['type'] == 'dynamicbundle' or item_dict['type'] == 'staticbundle':item_dict['type'] = 'bundle'
+                    if item_dict['type'] == 'vmtpack':item_dict['type'] = 'pack'
+                    if item_dict['type'] == 'jamtrack':item_dict['type'] = 'jam-Track'
+                    if item_dict['type'] == 'legokits': item_dict['type'] = 'lego-Kit'
+
                     itemshop.append(item_dict)
-    MarkDown = open(f"Markdown/{getDate()}-ItemShop.md", "w", encoding="utf-8") 
-    filename = f"Markdown/{getDate()}-ItemShop.md"
+    return itemshop
+
+def commit(files) ->None:
+    repo = git.Repo('C:/Users/ewklu/OneDrive/Desktop/Github_Repos/Fortnite-Item-Shop-Historical')
+    repo.index.add(files)
+    origin = repo.remote(name='origin') 
+    existing_branch = repo.heads['main'] 
+    existing_branch.checkout() 
+    repo.index.commit(f"{getDate()} Item Shop")
+    print('Commited successfully, Pushing to Orgin') 
+    origin.push()
+    print("Completed Push to Orgin")
+    print("Closing app")
+
+def write_markdown(file, type,prefix, item, date, include_image=True, include_url=True):
+    image_md = f"![Image of {item['name']}](../images/{date}/{item['imageLocal']})" if include_image else ''
+    url_md = f"[Link to {item['name']} in the webshop](https://www.fortnite.com/item-shop/{item['type']}s/{item['urlExstension']}?lang=en-US)" if include_url else ''
+    file.write(f'\n### {prefix} {item["name"]} - {capFirst(type)}- {item["price"]} - Leaving: {item["outDate"]}\n{image_md}{url_md}')
+
+def handle_item(file, item, type,date):
+    prefix = "**NEW**" if checkSameDate(item['inDate']) else "**LEAVING NEXT ROTATION**" if checkSameDate(item['outDate']) else ""
+    
+    if item['image'] and item['urlExstension']:
+        write_markdown(file, type,prefix, item, date)
+    elif item['urlExstension']:
+        write_markdown(file, type, prefix, item, date, include_image=False)
+    elif item['image']:
+        write_markdown(file, type, prefix, item, date, include_url=False)
+
+def main()->None:
+    itemshop = scraper()
+    date = getDate()
+    filename = f"Markdown/{date}-ItemShop.md"
+    MarkDown = open(filename, "w", encoding="utf-8") 
     catagory = ""
     section = ""
     tempNumber = 1
-    for item in itemshop:
+    print('Compiling to Markdown and Downloading Images')
+    for item in tqdm(itemshop, total=len(itemshop)):
         if item['name'] == None or item['name'] == '':
             item['name'] = 'temp{}'.format(tempNumber)
             item['imageLocal'] = 'temp{}'.format(tempNumber) + '.png'
@@ -99,18 +133,25 @@ def scraping()->None:
         
                 
         if item['image'] != None:
-            imageDownload(item['image'], item['name'])
+            imageDownload(item['image'], item['name'], date)
+
+        if item['rmtImage'] != None:
+            imageDownload(item['rmtImage'], item['name'], date)
+        
+        if 'temp' in item['name'] :
+            item['name'] = 'Not Listed on Site'
         
         if item['type'] == 'Rmtpack':
-            MarkDown.write(f'\n### {item['name']} - Costs Real Money\n[Link to {item['name']} in the Epic Games Stor]({item['urlExstension']})')
+            #Link to {item['name']} in the Epic Games Store
+            MarkDown.write(f'\n### {item['name']} - Costs Real Money\n[![Image of {item['name']}](../images/{date}/{item['imageLocal']})]({item['urlExstension']})')
 
         elif item['image'] != None and item['urlExstension'] != None:
             if checkSameDate(item['inDate']):
-                MarkDown.write(f'\n### **NEW** {item['name']} - {item['price']} -  Leaving: {item['outDate']}\n[![Image of {item['name']}](../images/{getDate()}/{item['imageLocal']})](https://www.fortnite.com/item-shop/{item['type'] + 's'}/{item['urlExstension']}?lang=en-US)')
+                MarkDown.write(f'\n### **NEW** {item['name']} - {item['price']} -  Leaving: {item['outDate']}\n[![Image of {item['name']}](../images/{date}/{item['imageLocal']})](https://www.fortnite.com/item-shop/{item['type'] + 's'}/{item['urlExstension']}?lang=en-US)')
             elif checkSameDate(item['outDate']):
-                MarkDown.write(f'\n### **LEAVING NEXT ROTATION** {item['name']} - {item['price']} -  Leaving: {item['outDate']}\n[![Image of {item['name']}](../images/{getDate()}/{item['imageLocal']})](https://www.fortnite.com/item-shop/{item['type'] +'s'}/{item['urlExstension']}?lang=en-US)')
+                MarkDown.write(f'\n### **LEAVING NEXT ROTATION** {item['name']} - {item['price']} -  Leaving: {item['outDate']}\n[![Image of {item['name']}](../images/{date}/{item['imageLocal']})](https://www.fortnite.com/item-shop/{item['type'] +'s'}/{item['urlExstension']}?lang=en-US)')
             else:
-                MarkDown.write(f'\n### {item['name']} - {item['price']} -  Leaving: {item['outDate']}\n[![Image of {item['name']}](../images/{getDate()}/{item['imageLocal']})](https://www.fortnite.com/item-shop/{item['type'] + 's'}/{item['urlExstension']}?lang=en-US)')
+                MarkDown.write(f'\n### {item['name']} - {item['price']} -  Leaving: {item['outDate']}\n[![Image of {item['name']}](../images/{date}/{item['imageLocal']})](https://www.fortnite.com/item-shop/{item['type'] + 's'}/{item['urlExstension']}?lang=en-US)')
         
         elif item['image'] == None and item['urlExstension'] != None:
             if checkSameDate(item['inDate']):
@@ -122,28 +163,16 @@ def scraping()->None:
         
         elif item['image'] != None and item['urlExstension'] == None:
             if checkSameDate(item['inDate']):
-                MarkDown.write(f'\n### **NEW** {item['name']} - {item['price']} - {item['type']} -  Leaving: {item['outDate']}\n![Image of {item['name']}](../images/{getDate()}/{item['imageLocal']})')
+                MarkDown.write(f'\n### **NEW** {item['name']} - {item['price']} - {item['type']} -  Leaving: {item['outDate']}\n![Image of {item['name']}](../images/{date}/{item['imageLocal']})')
             elif checkSameDate(item['outDate']):
-                MarkDown.write(f'\n### **LEAVING NEXT ROTATION** {item['name']} - {item['price']} -  Leaving: {item['outDate']}\n![Image of {item['name']}](../images/{getDate()}/{item['imageLocal']})')
+                MarkDown.write(f'\n### **LEAVING NEXT ROTATION** {item['name']} - {item['price']} -  Leaving: {item['outDate']}\n![Image of {item['name']}](../images/{date}/{item['imageLocal']})')
             else:
-                MarkDown.write(f'\n### {item['name']} - {item['price']} - {item['type']} -  Leaving: {item['outDate']}\n![Image of {item['name']}](../images/{getDate()}/{item['imageLocal']})')
+                MarkDown.write(f'\n### {item['name']} - {item['price']} - {item['type']} -  Leaving: {item['outDate']}\n![Image of {item['name']}](../images/{date}/{item['imageLocal']})')
     MarkDown.close()
     
     print("Completed Compiling Markdown File, Adding to Repo")
-    
-    repo = git.Repo('C:/Users/ewklu/OneDrive/Desktop/Github_Repos/Fortnite-Item-Shop-Historical')
-    repo.index.add([filename, updateReadMe(), 'images/' + f'{getDate()}'])
-    origin = repo.remote(name='origin') 
-    existing_branch = repo.heads['main'] 
-    existing_branch.checkout() 
-    repo.index.commit(f"{getDate()} Item Shop")
-    
-    print('Commited successfully, Pushing to Orgin') 
-    
-    origin.push() 
-    
-    print("Completed Push to Orgin")
-    print("Closing app")
+    filesCommit = [filename, updateReadMe(date), f'images/{date}/']
+    commit(filesCommit)
     exit()
 
-scraping()
+main()
